146401286124b8f18218021436a7af1ceb0be02f
Andrew Chase <asperous2@gmail.com>
2013-04-03 20:13:46 -0700

    Forgot to initialize current_thread

--- policyserver.c
+++ policyserver.c
@@ -4,19 +4,19 @@
 #include <sys/socket.h>
 #include <pthread.h>
 
-#define MY_PORT		843
-#define MAXBUF		1028
-#define NUM_THREADS     1
+#define MY_PORT     843
+#define MAXBUF      1028
+#define NUM_THREADS     2
 
-int pipes[NUM_THREADS][2];
+int pipes[1][2];
 int sockfd;
 
-char[] policy =
-"<?xml version=\"1.0\"?>\n \
-<!DOCTYPE cross-domain-policy SYSTEM \"/xml/dtds/cross-domain-policy.dtd\">\n
-<cross-domain-policy>\n \
-<site-control permitted-cross-domain-policies=\"master-only\"/>\n \
-<allow-access-from domain=\"*\" to-ports=\"*\" />\n</cross-domain-policy>\r\n";
+const char *policy =
+"<?xml version=\"1.0\"?>\n"
+"<!DOCTYPE cross-domain-policy SYSTEM \"/xml/dtds/cross-domain-policy.dtd\">\n"
+"<cross-domain-policy>\n"
+"<site-control permitted-cross-domain-policies=\"master-only\"/>\n"
+"<allow-access-from domain=\"*\" to-ports=\"*\" />\n</cross-domain-policy>\r\n";
 
 void *SendPolicy(void *threadid) {
    int ok, clientfd;
@@ -24,33 +24,33 @@ void *SendPolicy(void *threadid) {
    char buffer[MAXBUF];
    tid = (long) threadid;
    while(1) {
-   	/* --- Wait for, and recieve a client ---- */
-	read(pipes[tid][0], &clientfd, sizeof(int), 0);
-	
-	/* --- Recieve --- */
-	recv(clientfd, buffer, 128, 0);
-	
-	/* --- Send --- */
-	send(clientfd, &policy, 250, 0);
-	
-	/* --- Close ---*/
-	close(clientfd);
+    /* --- Wait for, and recieve a client ---- */
+    read(pipes[tid][0], &clientfd, sizeof(int), 0);
+
+    /* --- Recieve --- */
+    recv(clientfd, buffer, 128, 0);
+
+    /* --- Send --- */
+    send(clientfd, &policy, 250, 0);
+
+    /* --- Close ---*/
+    close(clientfd);
    }
 }
 
 int main(int argc, char *argv[]) {
     /* --- Create worker threads --- */
     pthread_t threads[NUM_THREADS];
-    int t, current_thread;
+    int t;
     for(t=0; t<NUM_THREADS; t++) {
-	pipe(pipes[t]); // <- Create pipes for sending clients
-	pthread_create(&threads[t], NULL, SendPolicy, (void *)t);
+    	pipe(pipes[t]); // <- Create pipes for sending clients
+    	pthread_create(&threads[t], NULL, SendPolicy, (void *)t);
     }
-    
+
     /* --- Create streaming socket --- */
     if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)  {
-	perror("Socket");
-	return 1;
+    	perror("Socket");
+    	return 1;
     }
 
     /* --- Initialize address/port structure --- */
@@ -62,30 +62,31 @@ int main(int argc, char *argv[]) {
 
     /* --- Assign a port number to the socket --- */
     if (bind(sockfd, (struct sockaddr*) &self, sizeof(self)) != 0 ) {
-	perror("Error: socket bind");
-	return 1;
+    	perror("Error: socket bind");
+    	return 1;
     }
 
     /* --- Make it a "listening socket" --- */
     if ( listen(sockfd, 20) != 0 )    {
-	perror("socket--listen");
-	return 1;
+    	perror("socket--listen");
+    	return 1;
     }
 
     struct sockaddr_in client_addr;
     int addrlen=sizeof(client_addr);
     int clientfd;
+    int current_thread = 0;
     while (1) {
-    	/* --- Accept a client --- */
-	clientfd = accept(sockfd, (struct sockaddr*)&client_addr, &addrlen);
-	
-	/* --- Send the client to a worker thread --- */
-	write(pipes[current_thread][1], &clientfd, sizeof(int), 0);
-	
-	/* --- Cycle the threads --- */
-	current_thread++;
-	if (current_thread == NUM_THREADS)
-	    current_thread = 0;
+        /* --- Accept a client --- */
+    clientfd = accept(sockfd, (struct sockaddr*)&client_addr, &addrlen);
+
+    /* --- Send the client to a worker thread --- */
+    write(pipes[current_thread][1], &clientfd, sizeof(int), 0);
+
+    /* --- Cycle the threads --- */
+    current_thread++;
+    if (current_thread == NUM_THREADS)
+        current_thread = 0;
     }
 
     /* --- Clean up --- */
