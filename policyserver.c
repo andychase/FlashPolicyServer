561992a8b0bed33366e156b16b7b50a5de941be6
asperous <asperous2@gmail.com>
2013-09-11 13:23:13 -0400

    Add License

--- policyserver.c
+++ policyserver.c
@@ -1,3 +1,22 @@
+/* Flash Policy Server - The MIT License (MIT) - Copyright (c) 2013 Andrew Chase
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE. */
 #include <stdio.h>
 #include <arpa/inet.h>
 #include <errno.h>
@@ -20,24 +39,24 @@ const char *policy =
 "<allow-access-from domain=\"*\" to-ports=\"*\" />\n</cross-domain-policy>\r\n";
 
 void *SendPolicy(void *threadid) {
-   int clientfd;
-   int policy_size = strlen(policy);
-   int tid;
-   char buffer[MAXBUF];
-   tid = (long) threadid;
-   while(1) {
-    /* --- Wait for, and recieve a client ---- */
-    read(pipes[tid][0], &clientfd, sizeof(int), 0);
+    int clientfd;
+    int policy_size = strlen(policy);
+    int tid;
+    char buffer[MAXBUF];
+    tid = (long) threadid;
+    while(1) {
+        /* --- Wait for, and recieve a client ---- */
+        read(pipes[tid][0], &clientfd, sizeof(int), 0);
 
-    /* --- Recieve --- */
-    recv(clientfd, buffer, MAXBUF, 0);
+        /* --- Recieve --- */
+        recv(clientfd, buffer, MAXBUF, 0);
 
-    /* --- Send --- */
-    send(clientfd, policy, policy_size, 0);
+        /* --- Send --- */
+        send(clientfd, policy, policy_size, 0);
 
-    /* --- Close ---*/
-    close(clientfd);
-   }
+        /* --- Close ---*/
+        close(clientfd);
+    }
 }
 
 int main(int argc, char *argv[]) {
@@ -45,14 +64,14 @@ int main(int argc, char *argv[]) {
     pthread_t threads[NUM_THREADS];
     int t;
     for(t=0; t<NUM_THREADS; t++) {
-       pipe(pipes[t]); // <- Create pipes for sending clients
-    	pthread_create(&threads[t], NULL, SendPolicy, (void *)t);
+        pipe(pipes[t]); // <- Create pipes for sending clients
+        pthread_create(&threads[t], NULL, SendPolicy, (void *)t);
     }
 
     /* --- Create streaming socket --- */
     if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)  {
-    perror("Socket");
-    return 1;
+        perror("Socket");
+        return 1;
     }
 
     /* --- Initialize address/port structure --- */
@@ -81,15 +100,15 @@ int main(int argc, char *argv[]) {
     int current_thread = 0;
     while (1) {
         /* --- Accept a client --- */
-    clientfd = accept(sockfd, (struct sockaddr*)&client_addr, &addrlen);
+        clientfd = accept(sockfd, (struct sockaddr*)&client_addr, &addrlen);
 
-    /* --- Send the client to a worker thread --- */
-    write(pipes[current_thread][1], &clientfd, sizeof(int), 0);
+        /* --- Send the client to a worker thread --- */
+        write(pipes[current_thread][1], &clientfd, sizeof(int), 0);
 
-    /* --- Cycle the threads --- */
-    current_thread++;
-    if (current_thread == NUM_THREADS)
-        current_thread = 0;
+        /* --- Cycle the threads --- */
+        current_thread++;
+        if (current_thread == NUM_THREADS)
+            current_thread = 0;
     }
 
     /* --- Clean up --- */
